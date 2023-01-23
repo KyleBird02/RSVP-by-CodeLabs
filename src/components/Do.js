7d31c561a7d0db3a320f0afeab5ad4d0ecc96ecf
Neave Paul <neave.mailbox@gmail.com>
2023-01-23 23:21:30 +0530

    All bits in place... now we have to build

--- src/components/Do.js
+++ src/components/Do.js
@@ -7,6 +7,22 @@ export default function Do() {
   return (
     <div className="screen">
       <div className="rsvp-img kyle-rsvp-img"></div>
+      <div className="graphic-dos"></div>
+      <h2>
+        Here's a list of things you <span className="green-will">will</span>{" "}
+        get:
+      </h2>
+      <ul>
+        <li>
+          <span className="tick">&#10004;</span> 1 Lakh+ Prize Pool
+        </li>
+        <li>
+          <span className="tick">&#10004;</span> Free Wi-Fi for 24 Hours
+        </li>
+        <li>
+          <span className="tick">&#10004;</span> Free Food for Everyone
+        </li>
+      </ul>
       <button
         className="button kyle-button"
         onClick={() => {
