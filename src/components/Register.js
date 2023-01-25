e50d5ac09c8a812774cae930c3bfaa58fea81362
LAPTOP-0BE72HIK\afifk <afifkhanab@gmail.com>
2023-01-25 21:49:30 +0530

    css fixed final

--- src/components/Register.js
+++ src/components/Register.js
@@ -2,18 +2,21 @@ import React from "react";
 
 export default function Register() {
   return (
+    <a href="https://unscript2k23.devfolio.co/">
     <div className="screen">
       <div className="rsvp-img kyle-rsvp-img"></div>
       <h1 className='powered-by'>Powered By CodeLabs x Mozilla</h1>
       <div className="poster-image-maybe"></div>
-
-      <h2>Don't miss out on Unscript 2k23!</h2>
-      <p>
-        Join us for a weekend of fun, learning and networking with the best in
-        the industry.
-      </p>
-      <p>You won't regret it!</p>
+      <div className="dont-miss-out">
+        <h2>Don't miss out on Unscript 2k23!</h2>
+        <p>
+          Join us for a weekend of fun, learning and networking with the best in
+          the industry.
+        </p>
+        <p>You won't regret it!</p>
+      </div>
       <button className="button kyle-button">Register Now</button>
     </div>
+    </a>
   );
 }
